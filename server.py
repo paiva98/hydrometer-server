ce23e3fc52ce28376c4cc7727880fe17f5353cb4
paiva98 <nilsonpaiva98@gmail.com>
2023-08-16 08:27:10 -0300

    new changes

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 import http.server
 import socketserver
+import logging
+
 from PIL import Image
 import io
+import json
 
 PORT = 8000
 
@@ -16,20 +19,29 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_error(404)
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        print("aaaaaaaaaa", body)
-        image = Image.open(io.BytesIO(body))
-        
-        print(image)
+        if self.path == '/send':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            data = json.loads(post_data.decode('utf-8'))
 
-        # display(image)
-        
-        # self.send_response(200)
-        # self.send_header('Content-type', 'text/html')
-        # self.end_headers()
-        # self.wfile.write(b"Received: " + body)
+            # Extraia os valores do objeto JSON recebido
+            id = data['id']
+            valor = data['valor']
 
+            print(f'O hidrômetro {id} enviou o valor {valor}')
+
+            # Responda com um JSON de confirmação
+            response = {'status': 'success'}
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(response).encode('utf-8'))
+            
+        else:
+            self.send_error(404)
+            
+
+            
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
